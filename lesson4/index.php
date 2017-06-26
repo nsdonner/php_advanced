43190da004ab620f8ee087438029d32009ad3783
nsdonner <ns_donner@mail.ru>
2017-06-26 09:04:12 +0300

    lesson4  sql

--- lesson4/index.php
+++ lesson4/index.php
@@ -5,10 +5,6 @@
  * Date: 26.06.2017
  * Time: 7:40
  */
-
-
-
-
 ?>
 
 <!doctype html>
@@ -19,48 +15,33 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script
-        src="http://code.jquery.com/jquery-3.2.1.min.js"
-        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-        crossorigin="anonymous"></script>
+            src="http://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+            crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
 
 
-    <ul id="goods">
-        <?php
-        include 'query.php';
-
-
-        ?>
-
-    </ul>
-    <input id="more" value="MORE!" type="button" />
-
-    <script>
-
-    $(document).ready(function() {
-    // Здесь мы пропишим необходимый код
-
-       var i=1;
-       $("#more").click(function () {
-
-           i++;
-           $.post("query.php", {more:i}, function (data) {
-               $("#goods").html(data);
-           });
-
-           return false;
-
-       });
-
-
+<ul id="goods">
+    <?php
+    include 'query.php';
+    ?>
+</ul>
+<input id="more" value="MORE!" type="button"/>
+<script>
+    $(document).ready(function () {
+        // Здесь мы пропишим необходимый код
+        var i = 1;
+        $("#more").click(function () {
+            i++;
+            $.post("query.php", {more: i}, function (data) {
+                $("#goods").html(data);
+            });
+            return false;
+        });
     });
-
-
-    </script>
-
-
+</script>
 
 
 </body>
